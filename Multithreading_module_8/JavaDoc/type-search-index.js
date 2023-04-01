8e3a97c58e6340494c47ccf57f8dc79c8aa96bea
Nenad <nenad0711@gmail.com>
2023-03-31 22:12:26 -0400

    module_8_IntelliJ_update

---
+++ Multithreading_module_8/JavaDoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Computation"},{"p":"<Unnamed>","l":"Computation_Test"}];updateSearchResults();
