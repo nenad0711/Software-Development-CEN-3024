a256fa029b865e33ad3c7081d01cbbb989831448
Nenad <nenad0711@gmail.com>
2023-04-06 21:42:00 -0400

    cleanup

--- AS Program/Generics_Module_6/src/Valuable.java
+++
@@ -1,4 +0,0 @@
-public interface Valuable {
-    int returnValue();
-}
-
--- AS Program/Threads/src/Thread2.java
+++
@@ -1,8 +0,0 @@
-public class Thread2 {
-
-    public static void main(String[] args) {
-        ThreadTest3 thr = new ThreadTest3();
-        thr.start();
-
-    }
-}
